99f8828db8f4625706dae39a4a138212efd1e85e
gerstnir <gernir@gmail.com>
2026-06-08 11:39:10 +0300

    Add while loop examples and enhance shopping calculator functionality

---
+++ while_loop/while_if.py
@@ -0,0 +1,21 @@
+# number = int(input("Type a number: "))
+
+# while number > 0 :
+#     # print("number > 0", number > 0)
+#     # print(number)
+#     if number % 2 == 0 :
+#         print(number, "is zugi")
+#     number = number - 1
+
+# print("number =", number, "loop finished")
+
+#tirgul
+
+number = int(input("Type a number: "))
+
+while number > 0 :
+    if number % 3 == 0 :
+        print(number, "is mitchalek in 3")
+    number = number - 1
+
+print("number =", number, "loop finished")
--- while_loop/while_practical.py
+++ while_loop/while_practical.py
@@ -10,22 +10,33 @@
 #     print("*" * x)
 #     x = x-1
 
-# number guessing game
+# # number guessing game
 
-right_number = 84
+# right_number = 84
 
-guess = int( input("Guess  a number (1-100): ") )
+# guess = int( input("Guess  a number (1-100): ") )
 
-# איזו שאלה אני צריך לשאול כדי לבקש מספר נוסף כשהניחוש לא נכון?
-# התשובה לשאלה צריכה להיות "כן כשהניחוש שונה מהמספר הנכון"
-while guess != right_number :
-    print("Wrong !")
-    guess = int( input("Guess again (1-100): ") )
+# # איזו שאלה אני צריך לשאול כדי לבקש מספר נוסף כשהניחוש לא נכון?
+# # התשובה לשאלה צריכה להיות "כן כשהניחוש שונה מהמספר הנכון"
+# while guess != right_number :
+#     print("Wrong !")
+#     guess = int( input("Guess again (1-100): ") )
     
-print("You  are right !")
-
-
+# print("You  are right !")
 
 # tirgul
 summ = 0
-amount 
+amount = 0
+
+print("Welcome to the shopping calculator!\n")
+price = input("Type first price ('stop' to exit): ")
+
+while price != 'stop':
+    summ = summ + float(price)
+    amount = amount + 1
+    price = input("Type next price ('stop' to exit): ")
+
+# print("\nThe total price is: ", summ, 'NIS\n')
+print("\nThe total price is: ", summ, '₪\n')
+print("\nThe product amount is: ", amount, "Product\n")
+print("\nThe average price is: ", round(summ/amount,2), "\u00a3\n")
